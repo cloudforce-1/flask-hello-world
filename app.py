e43f84e061e5c578d00532227488d0088fd4708b
hz-cf1 <hassan.zaheer@cloudforce-1.com>
2025-08-28 15:06:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import streamlit as st
 
 
-st.write("Hello")
-
-st.title("Python App")
-
-
-st.header("WeB App")
-
-
-print("This message will only show up in your terminal.")
-
-
 name = "World"
 st.write(f"Hello, {name}!")
